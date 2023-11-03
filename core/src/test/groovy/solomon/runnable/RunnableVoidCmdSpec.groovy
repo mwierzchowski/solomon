be7e4840af2742225e93542757e77e9a341f93ec
Marcin Wierzchowski <mwierzch@gmail.com>
2023-11-03 22:54:34 +0100

    cleanup

--- core/src/test/groovy/solomon/runnable/RunnableVoidCmdSpec.groovy
+++ core/src/test/groovy/solomon/runnable/RunnableVoidCmdSpec.groovy
@@ -18,13 +18,6 @@ class RunnableVoidCmdSpec extends Specification {
         noExceptionThrown()
     }
 
-    def "Test that fails"() {
-        when:
-        def x = 1 + 1
-        then:
-        x == 3
-    }
-
 //    public static void main(String[] args) {
 //        new CommandExecutorJdk().runnable(RunnableVoidCmd.class)
 //                .initialize(cmd -> {
