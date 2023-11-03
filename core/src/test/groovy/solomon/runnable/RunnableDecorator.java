b670011257c6936faa3251ac2650e41fa2b0da11
Marcin Wierzchowski <mwierzch@gmail.com>
2023-11-03 22:55:19 +0100

    add SonarCloud (#3)
    * add SonarCloud
    
    * Cleanup build and test workflow
    
    * Cleanup sonar scan
    
    * Cleanup gradle validation
    
    * Add sonar cloud badge
    
    * fix not working java version in the console due to incorect alias
    
    * fix sonar configuration
    
    * minor formatting fix
    
    * Add tests
    
    * Add jacoco report aggregation
    
    * Add codecov upload
    
    * simplify build script
    
    * cleanup
    
    * add codecov badge
    
    * disable codecov explicit file location
    
    * bring back explocit coverage file location
    
    * add workflow dispatch to all pipelines
    
    * Test of failing test
    
    * cleanup
    
    * cleanup

---
+++ core/src/test/groovy/solomon/runnable/RunnableDecorator.java
@@ -0,0 +1,12 @@
+package solomon.runnable;
+
+class RunnableDecorator implements Runnable {
+//    private final Runnable cmd;
+
+    @Override
+    public void run() {
+//        System.out.println("=== CLASS DECORATOR ===");
+//        cmd.run();
+//        System.out.println("=======================");
+    }
+}
