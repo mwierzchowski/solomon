ff1087dcf2140d6db037d3b0c12dab544006579b
Marcin Wierzchowski <mwierzch@gmail.com>
2023-11-10 10:36:38 +0100

    Use auto configuration in test application

--- solomon-spring/src/test/groovy/solomon/spring/test/TestApplication.java
+++ solomon-spring/src/test/groovy/solomon/spring/test/TestApplication.java
@@ -1,8 +1,7 @@
 package solomon.spring.test;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import solomon.spring.CommandExecutorSpring;
 
-@SpringBootApplication(scanBasePackageClasses = CommandExecutorSpring.class)
+@SpringBootApplication
 public class TestApplication {
 }
