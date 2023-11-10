6a6bc1e3535bb3b4e1d2a901e7bdb211d9aacfcb
Marcin Wierzchowski <mwierzch@gmail.com>
2023-11-10 10:13:07 +0100

    Correct command annotation

--- solomon-spring/src/main/java/solomon/spring/Command.java
+++ solomon-spring/src/main/java/solomon/spring/Command.java
@@ -1,10 +1,23 @@
 package solomon.spring;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+@Documented
 @Inherited
-@Bean
+@Component
+@Scope(SCOPE_PROTOTYPE)
 public @interface Command {
 }
