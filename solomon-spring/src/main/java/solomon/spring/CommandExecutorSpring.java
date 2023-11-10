dec102f2111b63a96205215ee7b09ddeab34536c
Marcin Wierzchowski <mwierzch@gmail.com>
2023-11-10 10:36:21 +0100

    Add auto configuration

--- solomon-spring/src/main/java/solomon/spring/CommandExecutorSpring.java
+++ solomon-spring/src/main/java/solomon/spring/CommandExecutorSpring.java
@@ -2,10 +2,8 @@ package solomon.spring;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Service;
 import solomon.CommandExecutor;
 
-@Service
 @RequiredArgsConstructor
 public class CommandExecutorSpring extends CommandExecutor {
     private final ApplicationContext context;
---
+++ solomon-spring/src/main/java/solomon/spring/SolomonAutoConfiguration.java
@@ -0,0 +1,14 @@
+package solomon.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import solomon.CommandExecutor;
+
+@Configuration
+public class SolomonAutoConfiguration {
+    @Bean
+    public CommandExecutor commandExecutor(ApplicationContext applicationContext) {
+        return new CommandExecutorSpring(applicationContext);
+    }
+}
