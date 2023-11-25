04f9b54ac5c67a591970a1ac9f85cb829f012e65
Marcin Wierzchowski <mwierzch@gmail.com>
2023-11-25 16:58:31 +0100

    cleanup

--- solomon-core/src/main/java/solomon2/core/configs/ListConfig.java
+++ solomon-core/src/main/java/solomon2/core/configs/ListConfig.java
@@ -17,10 +17,10 @@ import static solomon2.core.Utils.cast;
 @Slf4j
 @Setter
 public class ListConfig implements Config {
-    public static final List<Class<? extends Addon>> SUPPORTED_ADDONS = asList(Decorator.class, Listener.class);
+    protected static final List<Class<? extends Addon>> SUPPORTED_ADDON_CLASSES = asList(Decorator.class, Listener.class);
 
-    private List<Decorator<?, ?>> decorators;
-    private List<Listener<?, ?>> listeners;
+    private List<Decorator<?, ?>> decoratorList;
+    private List<Listener<?, ?>> listenerList;
 
     @Override
     public <A extends Addon> A getAddon(Class<A> addonClass, int position) {
@@ -48,8 +48,8 @@ public class ListConfig implements Config {
 
     protected Class<? extends Addon> typeOf(Addon addon) {
         var addonClass = addon.getClass();
-        for (int i = 0; i < SUPPORTED_ADDONS.size(); i++) {
-            var supportedClass = SUPPORTED_ADDONS.get(i);
+        for (int i = 0; i < SUPPORTED_ADDON_CLASSES.size(); i++) {
+            var supportedClass = SUPPORTED_ADDON_CLASSES.get(i);
             if (supportedClass.isAssignableFrom(addonClass)) {
                 return supportedClass;
             }
@@ -60,19 +60,19 @@ public class ListConfig implements Config {
     protected List<Addon> addonList(Class<? extends Addon> addonClass, boolean create) {
         List<? extends Addon> list;
         if (addonClass == Decorator.class) {
-            if (this.decorators == null && create) {
-                this.decorators = new ArrayList<>();
+            if (this.decoratorList == null && create) {
+                this.decoratorList = new ArrayList<>();
             }
-            list = this.decorators;
+            list = this.decoratorList;
         } else if (addonClass == Listener.class) {
-            if (this.listeners == null && create) {
-                this.listeners = new ArrayList<>();
+            if (this.listenerList == null && create) {
+                this.listenerList = new ArrayList<>();
             }
-            list = this.listeners;
+            list = this.listenerList;
         } else {
             var message = MessageFormat.format("Class {0} is not supported addon", addonClass);
             throw new IllegalArgumentException(message);
         }
-        return list != null ? cast(list) : emptyList();
+        return list == null ?  emptyList() : cast(list);
     }
 }
