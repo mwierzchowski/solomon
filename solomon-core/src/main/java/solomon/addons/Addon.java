12e82cbefd86d0b06c609ce66d7b56ce652cae4d
Marcin Wierzchowski <mwierzch@gmail.com>
2023-12-03 10:10:03 +0100

    Adapt cache mode to spring model

--- solomon-core/src/main/java/solomon/addons/Addon.java
+++ solomon-core/src/main/java/solomon/addons/Addon.java
@@ -1,10 +1,14 @@
 package solomon.addons;
 
+import java.util.Objects;
+
+import static solomon.annotations.Addon.CacheMode.DEFAULT;
+
 public interface Addon {
     default boolean isCacheable() {
         var config = getClass().getAnnotation(solomon.annotations.Addon.class);
         if (config != null) {
-            return config.useCache();
+            return Objects.equals(config.cacheMode(), DEFAULT);
         } else {
             return true;
         }
--- solomon-core/src/main/java/solomon/annotations/Addon.java
+++ solomon-core/src/main/java/solomon/annotations/Addon.java
@@ -7,11 +7,17 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static solomon.annotations.Addon.CacheMode.DEFAULT;
 
 @Retention(RUNTIME)
 @Target(TYPE)
 @Documented
 @Inherited
 public @interface Addon {
-    boolean useCache() default true;
+    String cacheMode() default DEFAULT;
+
+    interface CacheMode {
+        String NONE = "none";
+        String DEFAULT = "default";
+    }
 }
